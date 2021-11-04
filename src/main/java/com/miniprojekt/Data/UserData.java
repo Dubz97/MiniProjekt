acf3efe72dc40495a503958bebe79984f0ae5f10
gnismas <csamsinges@gmail.com>
2021-11-04 14:29:55 +0100

    connection working

--- src/main/java/com/miniprojekt/Data/UserData.java
+++ src/main/java/com/miniprojekt/Data/UserData.java
@@ -9,32 +9,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
     public class UserData {
-//    public boolean setConnection() {
-//        final String url = "jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11448176";
-//        boolean res = false;
-//        try {
-//            Connection connection = DriverManager.getConnection(url, "sql11448176", "25eQXYp4yW");
-//            res = true;
-//            Statement stmt = connection.createStatement();
-//            String sql = "CREATE TABLE IF NOT EXISTS Accounts" +
-//                    " (username VARCHAR(255), " +
-//                    " password VARCHAR(255), " +
-//                    " fname varchar(255), " +
-//                    "lname varchar (255),"+
-//                    " PRIMARY KEY (id))";
-//            stmt.execute(sql);
-//            System.out.println("Created table in given database...");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            System.out.println("We didnt get connection, error: =" + e.getMessage());
-//        }
-//        return res;
-//    }
 
         public void postUserDetails(User user) {
             Connection conn = DBManager.getConnection();
 
-            String query = "insert into accounts(id, username, password, fname, lname) values (?,?,?,?,?)";
+            String query = "insert into accounts(username, password, fname, lname) values (?,?,?,?)";
             PreparedStatement preparedStatement;
             try {
                 preparedStatement = conn.prepareStatement(query);
